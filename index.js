cb7af370f716de1d740d1c188a8aeed83d69ca76
Jean-Francois <hafidjeanfrancois@gmail.com>
2023-10-24 22:12:48 +0200

    to fulfill perfectly the quest second objective with a 'log'

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.listen(port, (err) => {
 
 app.get("/", (req, res) => {
   res.send(
-    `I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}.`
+    console.log(
+      `I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}.`
+    )
   );
 });
